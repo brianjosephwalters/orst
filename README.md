ad1b0907ba5921fa9c1e197349343b99c453797e
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-11-21 11:57:24 -0600

    Insertion sort working.

--- README.md
+++ README.md
@@ -8,9 +8,27 @@
     * It uses the generic type to determine which Sorter to apply?
 * Create a test that utilizes this structure with sort from the rust libraries.
 
-## Bubblesort
+## Bubble Sort
 * Note that BubbleSort is a unit struct.  It doesn't have state.
 * slice.swap() internally uses mem-swap.  (Isn't creating a variable to hold references while swapping.)
 * Why require the _ in `super::sort::<_, BubbleSort>(&mut things);`?
     * We have two type parameters to this function.
     * Since we are trying to name the sorting type so we have to put place holders for all of the other generic types.
+
+## Insertion Sort
+* Why not `SorterName::sort(&mut things)` direclty without the freestanding function `super::sort::<_, BubbleSort>(&mut things);`?
+    * Downside is that you need to have the `Sorter` trait in scope. (Is this right?)
+* Can't use `PartialEq` because PartialEq doesn't let you order things.
+* Can't use `PartialOrd` if some elements can say "I don't know what the order should be here."
+* Everything beyond our threshold is unsorted, everything before it is sorted.
+    * Start with 1 because a list of 1 element is always sorted.
+* InsertionSort works best if you know it's a linked list.  Slices require shifting
+* Another way:
+    * [1 3 4 2 5]
+      hold the 2 in a variable
+      find where 2 goes
+      shift to the right by one, from there to where 2 was
+    * both end up being two swaps.
+* Why does binary search return `Ok()` and `Error()`?
+    * when returning index where the thing was found, uses Ok().
+    * when returning index where it should have been found, uses Err().
---
+++ src/insertionsort.rs
@@ -0,0 +1,43 @@
+use super::Sorter;
+
+pub struct InsertionSort;
+
+impl Sorter for InsertionSort {
+    fn sort<T>(slice: &mut [T]) where T: Ord {
+        // [ sorted | unsorted ]
+        for unsorted in 1..slice.len() {
+            let smart = true;
+            if !smart {
+                // slice[unsorted..] is unsorted
+                // take slice[unsorted] and place in sorted location in slice[..=unsorted]
+                // [ 1 3 4 | 2 ] - requires shifting
+                // easist way is to keep moving the element selected to the left until the next left element is smaller.
+                let i = unsorted;
+                while i > 0 && slice[i-1] > slice[i] {
+                    slice.swap(i-1, i);
+                    i -= 1;
+                }
+            } else {
+                // binary search tells you where the element *should* go on such a list.
+                let i = match slice[..unsorted].binary_search(&slice[unsorted]) {
+                    Ok(i) => i,
+                    Err(i) => i,
+                };
+                // can insert on a vector, not a slice
+                //slice.insert(i, &slice[unsorted]);
+                // rotate everything right starting at the insert location and ending at the element
+                // to be inserted.  Since rotate_right **wraps around**, you are moving the unsorted
+                // element into the ith position.
+                slice[i..=unsorted].rotate_right(1);
+            }
+            
+        }
+    }
+}
+
+#[test]
+fn insertionsort_works() {
+    let mut things = vec![4, 2, 5, 3, 1];
+    InsertionSort::sort(&mut things);
+    assert_eq!(things, &[1, 2, 3, 4, 5]);
+}
